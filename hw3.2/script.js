6160235c83f38584f0d8e48a0987f0c9a88c5470
Olga Lashuk <olga.lashuck@gmail.com>
2025-07-25 00:05:03 +0300

    hw3.2 done

---
+++ hw3.2/script.js
@@ -0,0 +1,9 @@
+let name = prompt("Enter your name: ");
+let surname = prompt("Enter your surname: ");
+let answer = prompt("Enter your answer: ");
+
+console.log(
+    `Name: ${name}
+    Surname: ${surname}
+    Answer: ${answer}`
+);
