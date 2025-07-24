5fb9f1b6d237586b2dce26ecc6a0fb87f2e191bb
Olga Lashuk <olga.lashuck@gmail.com>
2025-07-25 01:01:43 +0300

    in process

---
+++ classes/lesson4/app.js
@@ -0,0 +1,67 @@
+    //1
+// let firstNum = parseInt(prompt("Enter first number:"));
+// let secondNum = parseInt(prompt("Enter second number:"));
+
+// if (firstNum > secondNum) {
+//     console.log("The first number is bigger");
+// } else if (firstNum < secondNum) {
+//     console.log("The second number is bigger");
+// } else {
+//     console.log("The numbers are equal");
+// } 
+
+
+    //2
+// let firstDist = parseInt(prompt("Enter a first distance in km:")) * 1000;
+// let secondDist = parseInt(prompt("Enter a second distance in f:")) * 0.305;
+
+// console.log(`First distance: ${firstDist} m`);
+// console.log(`Second distance: ${secondDist} m`);
+
+// if (firstDist > secondDist) {
+//     console.log('The second distance is smaller');
+// } else if (firstDist < secondDist) {
+//     console.log('The first distance is smaller');
+// } else {
+//     console.log('Distances are equal');
+// }
+
+
+    //3
+// let a = parseInt(prompt("Enter a: "));
+// let b = parseInt(prompt("Enter b: "));
+
+// console.log(`a : ${a}`);
+// console.log(`b : ${b}`);
+
+// if (a > b) {
+//     console.log(a % b == 0 ? "'b' is divisor of a number 'a'" : "'b' is not divisor of a number 'a'");
+// } else if (a < b) {
+//     console.log(b % a == 0 ? "'a' is divisor of a number 'b'" : "'a' is not divisor of a number 'b'");
+// } else {
+//     console.log("Unexpected situation");
+// }
+
+
+    //4
+// let num = parseInt(prompt("Enter your number: "));
+// console.log(`Your number: ${num} `);
+
+// let lastDigit = num % 10;
+// console.log(`The last digit: ${lastDigit}`);
+
+// if (lastDigit % 2 == 0) {
+//     console.log("This is even number");
+// } else {
+//     console.log("This is odd number");
+// }
+
+
+    //5
+let doubleDigit = parseInt(prompt("Enter double digit:"));
+
+if (doubleDigit.lenth = 2) {
+
+} else {
+    
+}
