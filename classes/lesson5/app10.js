ab149ec8612ea0dbceb586d9a44beaaf7de00428
Olga Lashuk <olga.lashuck@gmail.com>
2025-07-31 14:17:25 +0200

    lesson 5 tasks done

--- classes/lesson5/app10.js
+++ classes/lesson5/app10.js
@@ -1,1 +1,9 @@
-//TODO
+let product = 0;
+
+for(let i = 1; i <= 10; i++) {
+    for(let j = 1; j <= 10; j++) {
+        product = i * j;
+        document.write(`${i} * ${j} = ${product}<br>`);
+    }
+    document.write(`<br><br>`);
+}
